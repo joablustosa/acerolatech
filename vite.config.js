0135e985d6286a2b2968f26bbf020361d7e3e024
Joab Lustosa <informaticajfx@gmail.com>
2026-05-29 11:11:30 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'build',
+  },
 })
 
